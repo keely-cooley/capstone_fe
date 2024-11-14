3a2948458bb5bc0ba049340928d7eed9e637c049
keely-cooley <keelymacloud@yahoo.com>
2024-11-14 06:32:51 -0600

    MovieDetails, back button

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function MovieDetails () {
     const { movieId } = useParams();
+    const navigate = useNavigate();
+
+    const previousPage = () => {
+        navigate(-1)
+    }
+
     return (
-        <div>
-            <h1>Movie Id { movieId }</h1>
+        <div className="movieDetailsContainer">
+            <div>
+            <h3>{ movieId }</h3>
+            <button className="button" onClick={previousPage}>Previous Page</button>
+            </div>
         </div>
     )
 }
